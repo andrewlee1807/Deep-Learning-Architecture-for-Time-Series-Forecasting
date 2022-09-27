bf498c0e634a5f85fa64e978cbdac13e50751a53
Andrew <andrewlee1807@gmail.com>
2022-09-27 23:50:42 +0900

    spain dataset added

--- utils/datasets.py
+++ utils/datasets.py
@@ -6,11 +6,13 @@ import pandas as pd
 # List dataset name
 cnu_str = "CNU"
 comed_str = "COMED"
+spain_str = "SPAIN"
 
 # Dataset path
 CONFIG_PATH = {
     cnu_str: "https://raw.githubusercontent.com/andrewlee1807/Weights/main/datasets/%EA%B3%B5%EB%8C%807%ED%98%B8%EA%B4%80_HV_02.csv",
-    comed_str: "https://raw.githubusercontent.com/andrewlee1807/Weights/main/datasets/COMED_hourly.csv"
+    comed_str: "https://raw.githubusercontent.com/andrewlee1807/Weights/main/datasets/COMED_hourly.csv",
+    spain_str: "https://raw.githubusercontent.com/andrewlee1807/Weights/main/datasets/spain/spain_ec_499.csv"
 }
 
 
@@ -40,7 +42,7 @@ class CNU(DataLoader):
         self.raw_data = self.read_a_single_sequence()
 
     def export_sequences(self):
-        return self.raw_data
+        return self.raw_data  # a single sequence
 
 
 # COMED_hourly
@@ -48,3 +50,14 @@ class COMED(DataLoader):
     def __init__(self, path_file=None):
         super(COMED, self).__init__(path_file, comed_str)
         self.dataframe = self.read_data_frame()
+
+
+# Spain dataset
+class SPAIN(DataLoader):
+    def __init__(self, path_file=None):
+        super(SPAIN, self).__init__(path_file, spain_str)
+        self.dataframe = self.read_data_frame()
+
+    def export_sequences(self):
+        # Pick the customer no 20
+        return self.dataframe.loc[:, 20]  # a single sequence
