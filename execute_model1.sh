48f0631de52c18efdbb990bc984c34a49554541e
Andrew <andrewlee1807@gmail.com>
2022-09-27 23:51:44 +0900

    add script to run model

---
+++ execute_model1.sh
@@ -0,0 +1,6 @@
+#
+# Copyright (c) 2022 Andrew
+# Email: andrewlee1807@gmail.com
+#
+
+python main.py --dataset_name="CNU" ----model_name="Model1" --history_len=24 --output_len=7 --num_features=1 --device=0
