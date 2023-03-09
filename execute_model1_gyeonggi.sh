cbb850c80e6fe2964cf2a2917982930e5663d0a3
Andrew <andrewlee1807@gmail.com>
2023-03-09 17:37:16 +0900

    add execute sh file

---
+++ execute_model1_gyeonggi.sh
@@ -0,0 +1,14 @@
+#
+# Copyright (c) 2022-2023 Andrew
+# Email: andrewlee1807@gmail.com
+#
+
+#python main.py --dataset_name="CNU" ----model_name="Model1" --history_len=24 --output_len=7 --num_features=1 --device=0
+
+for i in 12 13 14 15 16 17 18 19 20 21 22 23 24
+do
+   echo "Starting to train model with output length = $i"
+   python main.py --dataset_name="gyeonggi" --write_log_file=True --model_name="Model1" --config_path="config/gyeonggi_2955_1_hour.yaml" --output_length=$i --device=0 --output_dir="results"
+   echo "Finished training model with output length = $i"
+   echo "=================================================="
+done
