44d90edf5a68e26964e5c149d408454624c51916
Andrew <andrewlee1807@gmail.com>
2023-03-24 15:16:16 +0900

    sh script

--- execute_model1_cnu.sh
+++ execute_model1_cnu.sh
@@ -8,7 +8,7 @@
 for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24
 do
    echo "Starting to train model with output length = $i"
-   python main.py --dataset_name="gyeonggi" --write_log_file=True --model_name="Model1" --config_path="config/gyeonggi_2955_1_hour.yaml" --output_length=$i --device=0 --output_dir="results/gyeonggi_vanila_model_1"
+   python main.py --dataset_name="cnu" --write_log_file=True --model_name="Model1" --config_path="config/cnu_model_1.yaml" --output_length=$i --device=0 --output_dir="results/cnu_vanilla_model_1"
    echo "Finished training model with output length = $i"
    echo "=================================================="
 done
--- execute_model2_gyeonggi.sh
+++ execute_model2_gyeonggi.sh
@@ -8,7 +8,7 @@
 for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24
 do
    echo "Starting to train model with output length = $i"
-   python main.py --dataset_name="gyeonggi" --write_log_file=True --model_name="Model1" --config_path="config/gyeonggi_2955_1_hour.yaml" --output_length=$i --device=0 --output_dir="results/gyeonggi_vanila_model_1"
+   python main.py --dataset_name="gyeonggi" --write_log_file=True --model_name="Model2" --config_path="config/gyeonggi_2955_1_hour.yaml" --output_length=$i --device=0 --output_dir="results/gyeonggi_vanila_model_2"
    echo "Finished training model with output length = $i"
    echo "=================================================="
 done
