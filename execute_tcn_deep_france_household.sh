da0a98f1d038fdb5a89e9ac72d38f54cfda52435
Andrew <andrewlee1807@gmail.com>
2023-03-30 01:33:28 +0900

    add tcn sub-folder

--- execute_tcn_deep_france_household.sh
+++ execute_tcn_deep_france_household.sh
@@ -8,7 +8,7 @@
 for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24
 do
    echo "Starting to train model with output length = $i"
-   python main.py --dataset_name="FRANCE_HOUSEHOLD_HOUR" --write_log_file=True --model_name="TCN" --config_path="config/france_household_deep_layer.yaml" --output_length=$i --device=0 --output_dir="results/tcn/france_household_deep"
+   python main.py --dataset_name="FRANCE_HOUSEHOLD_HOUR" --write_log_file=True --model_name="TCN" --config_path="config/tcn/france_household_deep_layer.yaml" --output_length=$i --device=0 --output_dir="results/tcn/france_household_deep"
    echo "Finished training model with output length = $i"
    echo "=================================================="
 done
