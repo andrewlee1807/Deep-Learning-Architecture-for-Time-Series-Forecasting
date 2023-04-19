f5595ac470a1a6a25ad9fba15be95cb6be87b783
Andrew <andrewlee1807@gmail.com>
2023-04-19 16:23:24 +0900

    multivariable running

---
+++ shell_multivariable/execute_tcn_multi_eng7.sh
@@ -0,0 +1,17 @@
+#
+# Copyright (c) 2022-2023 Andrew
+# Email: andrewlee1807@gmail.com
+#
+
+for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24; do
+  echo "Starting to train model with output length = $i"
+  python main.py \
+          --dataset_name="CNU_ENGINEERING_7" \
+          --model_name="tcn" \
+          --config_path="multivariate_ts/config/baseline/building_7_lstm.yaml" \
+          --output_length=$i \
+          --device=0 \
+          --output_dir="results/multivariable/tcn/ENG_7"
+  echo "Finished training model with output length = $i"
+  echo "=================================================="
+done
