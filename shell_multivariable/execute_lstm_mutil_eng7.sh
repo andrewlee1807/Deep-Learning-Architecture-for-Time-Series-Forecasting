1d2ee9dc68339dc7311c16939322572e7c8fa41d
Andrew <andrewlee1807@gmail.com>
2023-04-19 16:28:50 +0900

    --write_log_file=True

--- shell_multivariable/execute_lstm_mutil_eng7.sh
+++ shell_multivariable/execute_lstm_mutil_eng7.sh
@@ -7,6 +7,7 @@ for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24; do
   echo "Starting to train model with output length = $i"
   python main.py \
           --dataset_name="CNU_ENGINEERING_7" \
+          --write_log_file=True \
           --model_name="lstm" \
           --config_path="multivariate_ts/config/baseline/building_7_lstm.yaml" \
           --output_length=$i \
