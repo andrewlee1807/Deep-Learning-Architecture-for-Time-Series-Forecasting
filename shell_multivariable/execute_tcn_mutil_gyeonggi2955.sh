666c4e010155fd12e539faf2a83c71cf6b8a59ea
Andrew <andrewlee1807@gmail.com>
2023-04-24 19:50:42 +0900

    script automaticaly run 24

--- shell_multivariable/execute_tcn_mutil_gyeonggi2955.sh
+++ shell_multivariable/execute_tcn_mutil_gyeonggi2955.sh
@@ -3,10 +3,11 @@
 # Email: andrewlee1807@gmail.com
 #
 
-for i in 1 24; do
+for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24; do
   echo "Starting to train model with output length = $i"
   python main.py \
           --dataset_name="GYEONGGI2955" \
+          --write_log_file=True \
           --model_name="tcn" \
           --config_path="multivariate_ts/config/baseline/gyeonggi_2955_lstm.yaml" \
           --output_length=$i \
