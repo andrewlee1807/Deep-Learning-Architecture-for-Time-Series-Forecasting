941a7938992496b37cf8272c7abec23982bae0f0
Andrew <andrewlee1807@gmail.com>
2023-05-19 15:31:58 +0900

    HO Bayesian ans Hyperband for TCN on CNU dataset

--- shell_multivariable/execute_tcn_Bay_multi_eng7.sh
+++ shell_multivariable/execute_tcn_Bay_multi_eng7.sh
@@ -5,15 +5,15 @@
 
 for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24; do
   echo "Starting to train model with output length = $i"
-  python main.py \
-          --dataset_name="CNU_ENGINEERING_7" \
-          --write_log_file=True \
-          --model_name="tcn" \
-          --config_path="multivariate_ts/config/baseline/building_7_lstm.yaml" \
-          --output_length=$i \
-          --device=0 \
-          --output_dir="results/multivariable/Bay/tcn/ENG_7" \
-          --search_mode="bayesian"
+  python main_tcn_multi_tuner.py \
+    --dataset_name="CNU_ENGINEERING_7" \
+    --write_log_file=True \
+    --model_name="tcn" \
+    --config_path="multivariate_ts/config/baseline/building_7_lstm.yaml" \
+    --output_length=$i \
+    --device=0 \
+    --output_dir="results/multivariable/Bay/tcn/ENG_7" \
+    --search_mode="bayesian"
   echo "Finished training model with output length = $i"
   echo "=================================================="
 done
