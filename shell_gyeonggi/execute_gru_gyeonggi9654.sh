e9dab65f21f71cae7d744b22052b0299fd3ffdee
Andrew <andrewlee1807@gmail.com>
2023-09-07 16:50:05 +0900

    update shell

--- shell_gyeonggi/execute_gru_gyeonggi9654.sh
+++ shell_gyeonggi/execute_gru_gyeonggi9654.sh
@@ -6,13 +6,13 @@
 for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 36 48 60 72; do
   echo "Starting to train model with output length = $i"
   python main.py \
-          --dataset_name="GYEONGGI2955" \
+          --dataset_name="GYEONGGI9654" \
           --write_log_file=True \
           --model_name="gru" \
-          --config_path="multivariate_ts/config/baseline/gyeonggi_2955_gru.yaml" \
+          --config_path="benchmark/config/gyeonggi_9654_gru.yaml" \
           --output_length=$i \
           --device=0 \
-          --output_dir="results/multivariable/gru/GYEONGGI2955"
+          --output_dir="benchmark/exp/gy/gru"
   echo "Finished training model with output length = $i"
   echo "=================================================="
 done
