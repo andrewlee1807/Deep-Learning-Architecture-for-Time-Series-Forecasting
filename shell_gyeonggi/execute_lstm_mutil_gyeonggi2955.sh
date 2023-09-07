4c2b2e8722d47ae622e481527b8c425cdf4aed68
Andrew <andrewlee1807@gmail.com>
2023-09-07 16:13:26 +0900

    GRU and LSTM benchmark for GY

---
+++ shell_gyeonggi/execute_lstm_mutil_gyeonggi2955.sh
@@ -0,0 +1,18 @@
+#
+# Copyright (c) 2022-2023 Andrew
+# Email: andrewlee1807@gmail.com
+#
+
+for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 36 48 60 72; do
+  echo "Starting to train model with output length = $i"
+  python main.py \
+          --dataset_name="GYEONGGI9654" \
+          --write_log_file=True \
+          --model_name="lstm" \
+          --config_path="benchmark/config/gyeonggi_9654_lstm.yaml" \
+          --output_length=$i \
+          --device=0 \
+          --output_dir="benchmark/exp/gy/lstm"
+  echo "Finished training model with output length = $i"
+  echo "=================================================="
+done
