b71d164bbed3a7fc4b62a6a250f958e3109692a4
Andrew <andrewlee1807@gmail.com>
2023-09-11 17:18:32 +0900

    LSTM and GRU on spain

---
+++ shell_spain/execute_gru_spain.sh
@@ -0,0 +1,19 @@
+#
+# Copyright (c) 2022-2023 Andrew
+# Email: andrewlee1807@gmail.com
+#
+
+for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 36 48 60 72; do
+  echo "Starting to train model with output length = $i"
+  python main.py \
+    --dataset_name="spain" \
+    --write_log_file=True \
+    --model_name="gru" \
+    --config_path="benchmark/config/spain/spain_gru.yaml" \
+    --output_length=$i \
+    --device=0 \
+    --output_dir="benchmark/exp/spain/gru"
+
+  echo "Finished training model with output length = $i"
+  echo "=================================================="
+done
