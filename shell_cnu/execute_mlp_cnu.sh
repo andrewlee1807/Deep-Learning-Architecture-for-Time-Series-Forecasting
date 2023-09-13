a128b630b5f7beb27eb360814320e1adc6673749
Andrew <andrewlee1807@gmail.com>
2023-09-13 18:19:36 +0900

    MLP long on CNU

--- shell_cnu/execute_mlp_cnu.sh
+++ shell_cnu/execute_mlp_cnu.sh
@@ -3,7 +3,7 @@
 # Email: andrewlee1807@gmail.com
 #
 
-for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 36 48 60 72; do
+for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 36 48 60 72 84 96 108 120 132 144; do
   echo "Starting to train model with output length = $i"
   python main.py \
     --dataset_name="CNU" \
