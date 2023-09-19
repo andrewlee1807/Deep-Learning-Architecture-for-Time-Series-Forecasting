7705a8561a903a38c852588c83cf68327ba788b5
Andrew <andrewlee1807@gmail.com>
2023-09-19 23:14:08 +0900

    stridetcn on france

---
+++ shell_france/execute_stridetcn_france.sh
@@ -0,0 +1,19 @@
+#
+# Copyright (c) 2022-2023 Andrew
+# Email: andrewlee1807@gmail.com
+#
+
+for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 36 48 60 72 84 96 108 120 132 144; do
+  echo "Starting to train model with output length = $i"
+  #   python main.py --dataset_name="GYEONGGI9654" --write_log_file=True --model_name="TCN" --config_path="config/tcn/gyeonggi_vanilla.yaml" --output_length=$i --device=0 --output_dir="results/gyeonggi/tcn/gyeonggi9654_vanilla"
+  python stridetcn.py \
+    --dataset_name="FRANCE_HOUSEHOLD_HOUR" \
+    --write_log_file=True \
+    --model_name="stridetcn" \
+    --config_path="benchmark/config/france/france_stridetcn.yaml" \
+    --output_length=$i \
+    --device=0 \
+    --output_dir="benchmark/exp/france/stridetcn"
+  echo "Finished training model with output length = $i"
+  echo "=================================================="
+done
